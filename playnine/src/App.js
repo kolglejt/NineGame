a0ab7a3d8db3b35af42ee1d6369ba6585d11ddc7
Aleksandra Pochec <aleksandra.pochec@hotmail.com>
2019-01-12 22:36:46 +0100

    the main components

--- playnine/src/App.js
+++ playnine/src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+library.add(faStar);
+
+const Stars = (props) => {
+  return(
+
+      <div className='col-5'>
+          <FontAwesomeIcon icon="star" />
+          <FontAwesomeIcon icon="star" />
+          <FontAwesomeIcon icon="star" />
+          <FontAwesomeIcon icon="star" />
+      </div>
+
+  );
+};
+const Button = (props) => {
+    return(
+
+        <div className='col-2'>
+          <button>=</button>
+        </div>
+
+    );
+};
+const Answer = (props) => {
+    return(
+
+        <div className='col-5'>
+          ....
+        </div>
+
+    );
+};
+const Numbers = (props) => {
+    return (
+        <div className='card text-center'>
+            <div>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+            </div>
+        </div>
+
+
+    )
+}
+class Game extends React.Component {
+    render () {
+        return (
+            <div className='container'>
+                <hr />
+                <h3>Play Nine</h3>
+                <div className="row">
+
+                    <Stars/>
+              <Button/>
+              <Answer/>
+                </div>
+                <br />
+                <Numbers/>
+            </div>
+        );
+    }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <Game/>
+
       </div>
     );
   }
